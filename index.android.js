9ccee4d7726048547f329eda9fa412c77f7c3338
eccentricayman <aahmed3@stuy.edu>
2017-07-10 00:24:39 -0400

    added android bundle and made more toolbar progress

--- index.android.js
+++ index.android.js
@@ -5,20 +5,30 @@ import {
 	AppRegistry,
 	StyleSheet,
 	Text,
+	Image,
+	Button,
+	TouchableNativeFeedback,
 	View
 } from 'react-native';
 
 //material design
-import { Toolbar, COLOR, ThemeProvider } from 'react-native-material-ui';
+import {
+	Toolbar,
+	COLOR,
+	ThemeProvider,
+	Icon,
+	ActionButton
+} from 'react-native-material-ui';
+
 const uiTheme = {
-    palette: {
-        primaryColor: COLOR.green500,
-    },
-    toolbar: {
-        container: {
-            height: 50,
-        },
-    },
+	palette: {
+		primaryColor: COLOR.white
+	},
+	toolbar: {
+		container: {
+			height: 50
+		}
+	}
 };
 
 // Initialize Firebase
@@ -26,11 +36,11 @@ import data from './keys.json';
 
 const firebaseConfig = {
 	apiKey: data['api-key'],
-    authDomain: data['authentication-domain'],
-    databaseURL: data["database-url"],
-    projectId: data['project-id'],
-    storageBucket: data['storage-bucket'],
-    messagingSenderId: data['messaging-sender-id']
+	authDomain: data['authentication-domain'],
+	databaseURL: data["database-url"],
+	projectId: data['project-id'],
+	storageBucket: data['storage-bucket'],
+	messagingSenderId: data['messaging-sender-id']
 };
 firebase.initializeApp(firebaseConfig);
 
@@ -39,39 +49,62 @@ const rootRef = firebase.database().ref();
 const itemsRef = rootRef.child('items');
 
 export default class Jot extends Component {
-  render() {
-      return (
-		  <ThemeProvider uiTheme={uiTheme}>
-			  <View style={styles.container}>
-				  <Toolbar
-					  leftElement="menu"
-					  centerElement="Searchable"
-					  searchable={{
-						  autoFocus: true,
-						  placeholder: 'Search',
-					  }} />
-			  </View>
-		  </ThemeProvider>
-      );
-  }
+	render() {
+		return (
+			<ThemeProvider uiTheme={uiTheme}>
+				<View style={styles.container}>
+				<Toolbar
+					leftElement = {
+						<TouchableNativeFeedback
+							onPress={this._onPressButton}
+							useForeground={true}
+							background={TouchableNativeFeedback.Ripple('#fff', true)}>
+							<View style={{width: 50, height: 50, alignItems: "center", justifyContent: "center"}}>
+								<Image
+								source={require('./static/default_user.png')}
+								style={styles.userImage}>
+								</Image>
+							</View>
+						</TouchableNativeFeedback>
+					}
+					centerElement={ <Text style={styles.title}>Jot</Text> }
+					searchable = {{
+						autoFocus: true,
+						placeholder: 'Search'
+					}}
+					style = {{
+						rightElement: { color: COLOR.grey900 }
+					}}></Toolbar>
+
+					<ActionButton
+						style = {{
+							container: { backgroundColor: COLOR.grey900 },
+							icon: { fontSize: 28 }
+						}}
+					/>
+				</View>
+			</ThemeProvider>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		backgroundColor: '#FAFAFA'
+	},
+	title: {
+		color: "#212121",
+		fontWeight: "500",
+		fontSize: 22
+	},
+	userImage: {
+		borderRadius: 500,
+		width: 30,
+		height: 30,
+		marginLeft: 10
+	}
 });
 
 AppRegistry.registerComponent('Jot', () => Jot);
